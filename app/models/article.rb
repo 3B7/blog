1a2b56cf0987be4bea86c9c21513a864171db9cf
erik batres <erikbatres@gmail.com>
2014-08-07 13:17:34 -0700

    cleaned up missing code and bugs

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
 
-	has_many :comments
+	has_many :comments, dependent: :destroy
+	##dependent destroy helps to delete associated objects. otherwise
+	##we'd have comments occupying space in the database. 
 	
 	validates :title, presence: true, 
 					length: {minimum: 5}
